33ba35b7fcc156890dbe95e216d1ec8d57afd3b5
ArjunVaskale <arjunvaskale10@gmail.com>
2020-10-26 15:16:42 +0530

    practical API Module

---
+++ updates/api/views.py
@@ -0,0 +1,19 @@
+
+from updates.models import Update as UpdateModel
+
+
+class UpdateModelDetailAPIViews():
+
+    def get( self  , request , *args , **kwargs):
+        return #json
+
+    def post( self  , request , *args , **kwargs):
+        return #json
+
+
+
+    def put( self  , request , *args , **kwargs):
+        return #json
+
+    def delete( self  , request , *args , **kwargs):
+        return #json
