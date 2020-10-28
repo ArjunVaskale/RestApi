b5571166e9ea682ce244057f82be261bcd67a123
ArjunVaskale <arjunvaskale10@gmail.com>
2020-10-29 01:41:29 +0530

    Http Status Codes

--- restapi/mixins.py
+++ restapi/mixins.py
@@ -1,4 +1,16 @@
-from django.http import JsonResponse
+from django.http import JsonResponse , HttpResponse 
+
+
+class HttpResponseMixin(object):
+	is_json = False 
+
+	def render_to_response(self , data , status = 200):
+		content_type = 'text/html'
+		if self.is_json:
+			content_type = 'application/json'
+		return HttpResponse(data ,content_type = content_type , status = status)
+
+
 
 
 class JsonResponseMixin(object):
--- updates/api/views.py
+++ updates/api/views.py
@@ -6,36 +6,49 @@ from updates.models import Update as UpdateModel
 
 from .mixins import CSRFExemptMixin
 
+from restapi.mixins import HttpResponseMixin 
+
 import json 
 
 
 
-class UpdateModelDetailAPIViews(CSRFExemptMixin ,View):
+class UpdateModelDetailAPIViews(HttpResponseMixin ,CSRFExemptMixin ,View):
+
+    is_json = True
+
     def get( self  , request , id , *args , **kwargs):
         obj = UpdateModel.objects.get(id=id)
         json_data = obj.serialize() 
-        return HttpResponse( json_data , content_type='application/json')
+        return self.render_to_response(json_data)
 
     def post( self  , request , *args , **kwargs):
-        return HttpResponse( json_data , content_type='application/json')
+        json_data = {}
+        return self.render_to_response(json_data)
 
 
     def put( self  , request , *args , **kwargs):
-        return HttpResponse( json_data , content_type='application/json')
+        json_data = {}
+        return self.render_to_response(json_data)
 
     def delete( self  , request , *args , **kwargs):
-        return HttpResponse( json_data , content_type='application/json')
+        json_data = {}
+        return self.render_to_response(json_data , status = 403)
+ 
+class UpdateModelListAPIViews(HttpResponseMixin ,CSRFExemptMixin ,View):
 
-class UpdateModelListAPIViews(CSRFExemptMixin ,View):
+
+    is_json = True
+
+
     def get( self  , request , *args , **kwargs):
         qs = UpdateModel.objects.all()
         json_data = qs.serialize() 
-        return HttpResponse( json_data , content_type='application/json')
+        return self.render_to_response(json_data)
 
     def post( self, request, *args, **kwargs):
         data = json.dumps({"message": "unknown data "})
-        return HttpResponse( data , content_type='application/json' )
+        return self.render_to_response(data , status= 400)
 
     def delete( self  , request , *args , **kwargs):
         data = json.dumps({"message": "You can't delete data "})
-        return HttpResponse( data , content_type='application/json')
+        return self.render_to_response(data , status= 403)
